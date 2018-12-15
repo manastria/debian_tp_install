fc101baff038d114070c78dfa2b9c96e032cbcdf
jpdemory <jpdemory@manastria.fr>
2018-12-15 20:16:57 +0100

    Backup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # git clone https://github.com/manastria/debian_tp_install.git
+# yadm clone https://manastria@bitbucket.org/manastria/dotfile.git
---
+++ install_rngtools.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+echo -e "\033[34m===============================================================================\033[0m"
+echo "Reconfigure openssh-server"
+echo -e "\033[34m===============================================================================\033[0m"
+
+cat > /etc/default/rng-tools << EOF
+# Configuration for the rng-tools initscript
+
+# This is a POSIX shell fragment
+
+# Set to the input source for random data, leave undefined
+# for the initscript to attempt auto-detection.  Set to /dev/null
+# for the viapadlock driver.
+#HRNGDEVICE=/dev/hwrng
+#HRNGDEVICE=/dev/null
+HRNGDEVICE=/dev/urandom
+
+# Additional options to send to rngd. See the rngd(8) manpage for
+# more information.  Do not specify -r/--rng-device here, use
+# HRNGDEVICE for that instead.
+#RNGDOPTIONS="--hrng=intelfwh --fill-watermark=90% --feed-interval=1"
+#RNGDOPTIONS="--hrng=viakernel --fill-watermark=90% --feed-interval=1"
+#RNGDOPTIONS="--hrng=viapadlock --fill-watermark=90% --feed-interval=1"
+EOF
