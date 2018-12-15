4a6148933549f8a65f5b0d8bfe485c3368b580fa
jpdemory <jpdemory@manastria.fr>
2018-12-15 16:45:15 +0100

    Correct bugs

--- install_aptproxy.sh
+++ install_aptproxy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/bash
 
 echo -e "IP et port du proxy (example : 172.16.0.1:3128) ?"
 read -r IPPROXY
--- install_issue.sh
+++ install_issue.sh
@@ -1,11 +1,9 @@
-#/bin/sh
+#/bin/bash
 
 . /etc/os-release
 
-text="${NAME} ${VERSION} \\\\n \\l\n"
-text="${text}\n"
-text="${text}SSH : \\4{enp0s8}\n"
-
-
-echo -e ${text} > /etc/issue
-
+cat > /etc/issue << EOF
+${NAME} ${VERSION} \\\\n \\l
+${text}
+${text}SSH : \\4{enp0s8}
+EOF
