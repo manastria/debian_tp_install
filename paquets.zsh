50568877d126d9fd474c9f5bad9d9690bd2aaf14
jpdemory <jpdemory@manastria.fr>
2018-12-23 01:52:41 +0100

    Ajout des paquets

--- paquets.zsh
+++ paquets.zsh
@@ -1,4 +1,4 @@
-#/usr/bin/zsh
+#!/usr/bin/zsh
 
 cat > /etc/apt/sources.list <<EOF
 #deb cdrom:[Debian GNU/Linux 9.5.0 _Stretch_ - Official amd64 NETINST 20180714-10:25]/ stretch main
@@ -17,47 +17,35 @@ EOF
 
 
 bases=(
-aptitude
-bash-completion
-build-essential
-byobu
-ccze
-curl
-git
-gpgv2
-haveged
-less
-linux-headers-$(uname -r)
-lnav
-locales-all
-most
-multitail
-rng-tools
-screen
-screenfetch
-sudo
-tmux
-unzip
-vim
-yadm
-zip
-zsh
-)
-
-
-gnome=(
-gdm3                       # The display manager
-gnome-session
-gnome-shell
-gnome-keyring
-libpam-gnome-keyring
-gnome-control-center
-network-manager-gnome
-gnome-terminal
+    aptitude
+    bash-completion
+    build-essential
+    byobu
+    ccze
+    curl
+    git
+    gpgv2
+    haveged
+    less
+    linux-headers-$(uname -r)
+    lnav
+    locales-all
+    most
+    multitail
+    rng-tools
+    screen
+    screenfetch
+    sudo
+    tmux
+    unzip
+    vim
+    yadm
+    zip
+    zsh
 )
 
 gnome=(
-gnome-core
+    gnome-core
 )
 
 vmware=(
@@ -65,10 +53,63 @@ vmware=(
     open-vm-tools
 )
 
-paquets=(
-$bases
+lxc=(
+    lxc
+    bridge-utils
+    vlan
+    libvirt-daemon-system
+    libvirt-dev
+    libvirt-clients
+    debootstrap
+    vde2
+    openvswitch-switch
+    xfce4
+    galculator
+    open-vm-tools-desktop
+    terminator
+    wireshark
+    traceroute
+    nmap
+    zenmap
+    netcat
+    ipcalc
+    gip
+    etckeeper
+    glances
+    net-tools
 )
 
+other=(
+    iputils-ping
+    apt-file
+    command-not-found
+    perl
+    libnet-ssleay-perl
+    openssl
+    libauthen-pam-perl
+    libpam-runtime
+    libio-pty-perl
+    apt-show-versions
+    python
+)
+
+apache=(
+    apache2
+    php7.0
+    libapache2-mod-php7.0
+    php7.0-intl
+    php7.0-mcrypt
+    php7.0-ldap
+    php7.0-gd
+    php7.0-imagick
+    php7.0-pdo-sqlite
+    php7.0-sqlite3
+    php7.0-mysql
+)
+
+paquets=(
+    $bases
+)
 typeset -U paquets
 
 apt update
