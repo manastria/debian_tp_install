7787605652482eae1eac99050b0be5687a311724
jpdemory <jpdemory@manastria.fr>
2018-12-26 21:55:16 +0100

    Ajout base

---
+++ base.zsh
@@ -0,0 +1,58 @@
+#!/usr/bin/zsh
+
+cat > /etc/apt/sources.list <<EOF
+#deb cdrom:[Debian GNU/Linux 9.5.0 _Stretch_ - Official amd64 NETINST 20180714-10:25]/ stretch main
+
+deb http://debian.mirrors.ovh.net/debian/ stretch main contrib non-free
+deb-src http://debian.mirrors.ovh.net/debian/ stretch main contrib non-free
+
+deb http://security.debian.org/debian-security stretch/updates main contrib non-free
+deb-src http://security.debian.org/debian-security stretch/updates main contrib non-free
+
+# stretch-updates, previously known as 'volatile'
+deb http://debian.mirrors.ovh.net/debian/ stretch-updates main contrib non-free
+deb-src http://debian.mirrors.ovh.net/debian/ stretch-updates main contrib non-free
+EOF
+
+
+
+bases=(
+    aptitude
+    bash-completion
+    build-essential
+    byobu
+    ccze
+    curl
+    git
+    gpgv2
+    haveged
+    less
+    linux-headers-$(uname -r)
+    lnav
+    locales-all
+    most
+    multitail
+    rng-tools
+    screen
+    screenfetch
+    sudo
+    tmux
+    unzip
+    vim
+    yadm
+    zip
+    zsh
+)
+
+
+paquets=(
+    $bases
+)
+typeset -U paquets
+
+yadm clone git@bitbucket.org:manastria/dotfile.git
+yadm reset --hard origin/master
+
+
+apt update
+apt install -y $paquets
--- paquets.zsh
+++ paquets.zsh
@@ -1,21 +1,5 @@
 #!/usr/bin/zsh
 
-cat > /etc/apt/sources.list <<EOF
-#deb cdrom:[Debian GNU/Linux 9.5.0 _Stretch_ - Official amd64 NETINST 20180714-10:25]/ stretch main
-
-deb http://debian.mirrors.ovh.net/debian/ stretch main contrib non-free
-deb-src http://debian.mirrors.ovh.net/debian/ stretch main contrib non-free
-
-deb http://security.debian.org/debian-security stretch/updates main contrib non-free
-deb-src http://security.debian.org/debian-security stretch/updates main contrib non-free
-
-# stretch-updates, previously known as 'volatile'
-deb http://debian.mirrors.ovh.net/debian/ stretch-updates main contrib non-free
-deb-src http://debian.mirrors.ovh.net/debian/ stretch-updates main contrib non-free
-EOF
-
-
-
 bases=(
     aptitude
     bash-completion
