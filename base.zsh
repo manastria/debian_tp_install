9d001cbeeb5383d437ec4bd293a00fd3637fa70b
Jean-Philippe DEMORY <jpdemory@manastria.fr>
2019-01-02 23:45:47 +0100

    Correction bug

--- base.zsh
+++ base.zsh
@@ -14,12 +14,12 @@ deb http://debian.mirrors.ovh.net/debian/ stretch-updates main contrib non-free
 deb-src http://debian.mirrors.ovh.net/debian/ stretch-updates main contrib non-free
 EOF
 
-cat > /etc/apt/apt.conf.d/20norecommends <<EOF
-APT {
-  Install-Recommends "false";
-  Install-Suggests "false";
-};
-EOF
+#cat > /etc/apt/apt.conf.d/20norecommends <<EOF
+#APT {
+#  Install-Recommends "false";
+#  Install-Suggests "false";
+#};
+#EOF
 
 apt update
 
