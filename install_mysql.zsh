9b1afd96153a2eae0466fcd842e792ab9ebeaf57
jpdemory <jpdemory@manastria.fr>
2019-02-21 15:55:24 +0100

    Change MySQL Install

--- install_mysql.zsh
+++ install_mysql.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/zsh
 
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+#wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+apt install dirmngr
+apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
 dpkg -i mysql-apt-config_0.8.10-1_all.deb
 apt update
 apt install -y mysql-server
