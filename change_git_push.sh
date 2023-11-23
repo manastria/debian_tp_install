0fd5e8f36ca1d7d0c5857a097530b23b02f0c5d6
manastria <manastria@users.noreply.github.com>
2023-11-23 11:16:36 +0100

    Update repository URL

--- change_git_push.sh
+++ change_git_push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
-git remote set-url --push origin git@bitbucket.org:manastria/debian_tp_install.git
-yadm remote set-url --push origin git@bitbucket.org:manastria/dotfile.git
+git remote set-url origin https://github.com/manastria/debian_tp_install.git
+git remote set-url --push origin git@github.com:manastria/debian_tp_install.git
 
+yadm remote set-url origin https://github.com/manastria/dotfile.git
+yadm remote set-url --push origin git@github.com:manastria/dotfile.git
