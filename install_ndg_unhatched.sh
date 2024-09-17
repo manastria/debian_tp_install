2f76ee99766cac88c934e8743a90939b30afbfcd
jpdemory <manastria@users.noreply.github.com>
2024-09-17 19:50:43 +0200

    Refactor installation script for unhatched

--- install_ndg_unhatched.sh
+++ install_ndg_unhatched.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Fonction pour afficher les messages d'erreur en rouge si la sortie est un terminal
+function error_message {
+  if [ -t 1 ]; then
+    echo -e "\e[31m$1\e[0m" >&2
+  else
+    echo "$1" >&2
+  fi
+}
+
 # Vérification si le script est exécuté avec sudo
 if [ "$EUID" -ne 0 ]; then
-  echo "Veuillez exécuter ce script en tant que root ou avec sudo."
+  error_message "Veuillez exécuter ce script en tant que root ou avec sudo."
   exit 1
 fi
 
@@ -11,19 +20,26 @@ USERNAME="sysadmin"
 PASSWORD="netlab123"
 TAR_FILE="ndg_unhatched.tar.bz2"
 
+# Vérification si l'utilisateur existe déjà
+if id "$USERNAME" &>/dev/null; then
+  error_message "L'utilisateur $USERNAME existe déjà."
+  exit 1
+fi
+
 # Création de l'utilisateur avec son répertoire personnel
 useradd -m -s /bin/bash $USERNAME
 
 # Définition du mot de passe pour l'utilisateur
 echo "$USERNAME:$PASSWORD" | chpasswd
 
-# Décompression de l'archive dans le répertoire racine
-if [ -f "$TAR_FILE" ]; then
-  tar -x -C / -f "$TAR_FILE"
-else
-  echo "Le fichier $TAR_FILE n'existe pas dans le répertoire courant."
+# Vérification de l'existence de l'archive
+if [ ! -f "$TAR_FILE" ]; then
+  error_message "Le fichier $TAR_FILE n'existe pas dans le répertoire courant."
   exit 1
 fi
+
+# Décompression de l'archive dans le répertoire racine
+tar -x -C / -f "$TAR_FILE"
 
 # Changement de propriétaire des fichiers décompressés dans /home/sysadmin
 chown -R $USERNAME:$USERNAME /home/$USERNAME
@@ -32,4 +48,3 @@ chown -R $USERNAME:$USERNAME /home/$USERNAME
 apt update && apt install -y sl
 
 echo "Le script a été exécuté avec succès."
-
