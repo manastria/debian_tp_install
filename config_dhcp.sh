d25bc188bd53f8c0424fcde4679de19c44bb1cdc
manastria <manastria@users.noreply.github.com>
2026-09-04 22:07:16 +0200

    Supprime les fichiers obsolètes du dépôt (VS Code, sous-module AD, paquets binaires, configs Ventoy/DHCP)
    Ménage effectué par l'utilisateur : fichiers de configuration d'éditeur,
    sous-module git désormais vide, scripts et configurations dupliqués ou migrés
    vers d'autres dépôts, paquets .deb versionnés par erreur.

--- config_dhcp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
-chown root:root /etc/dhcp/dhcpd.conf
-chmod 0644 /etc/dhcp/dhcpd.conf
-
--- install_chkservice.sh
+++
@@ -1,6 +0,0 @@
-### Ne fonctionne pas encore
-### Erreur avec add-apt-repository
-
-$ sudo add-apt-repository ppa:linuxenko/chkservice
-$ sudo apt-get update
-$ sudo apt-get install chkservice
